fe1e3a0e42c73d5abf32409051ea3aeb3bbbdd9c
suyog-elaunch <122338893+SuyogEL@users.noreply.github.com>
2023-05-04 10:49:27 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+const Header = () => {
+  return (
+    <Box>Header</Box>
+  )
+}
+
+export default Header
---
+++ src/components/SignUp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+const SignUp = () => {
+  return (
+    <Box>SignUp</Box>
+  )
+}
+
+export default SignUp
